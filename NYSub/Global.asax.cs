b452f0c000fbb9c88ee04da0906fbbd252c27335
Daniel Kovaliov <danielflinx@gmail.com>
2015-11-05 13:22:00 +0200

    Cleaned project from unused stuff

--- NYSub/Global.asax.cs
+++ NYSub/Global.asax.cs
@@ -10,11 +10,8 @@ namespace NYSub
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
             JobScheduler.Start();
         }
     }
